b2666f80a0d4af087432417b3e401c6b4ccc2e72
hxun202 <kangsojin0918@gmail.com>
2026-03-19 21:44:05 +0900

    2026.3.19 벡터

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -11,71 +11,16 @@ private:
 	{
 		T data;
 		Node* next;
-		Node* previous;
 	};
 
 	int size;
 	Node* head;
-	Node* tail;
 
 public:
 	List()
 	{
 		size = 0;
 		head = nullptr;
-		tail = nullptr;
-	}
-
-	void push_front(T data)
-	{
-		Node* newNode = new Node;
-
-		newNode->data = data;
-		newNode->next = nullptr;
-		newNode->previous = nullptr;
-
-		if (head == nullptr)
-		{
-			head = newNode;
-			tail = newNode;
-		}
-		else
-		{
-			head->previous = newNode;
-
-			newNode->next = head;
-
-			head = newNode;
-		}
-
-		size++;
-	}
-
-	void pop_front()
-	{
-		if (head == nullptr)
-		{
-			cout << "linked list is empty" << endl;
-		}
-		else
-		{
-			Node * deleteNode = head;
-			if (head == tail)
-			{
-				head = nullptr;
-				tail = nullptr;
-			}
-			else
-			{
-				deleteNode->next->previous = nullptr;
-
-				head = head->next;
-			}
-
-			delete deleteNode;
-
-			size--;
-		}
 	}
 
 	void push_back(T data)
@@ -83,36 +28,34 @@ public:
 		Node* newNode = new Node;
 
 		newNode->data = data;
-		newNode->next = nullptr;
-		newNode->previous = nullptr;
 
-		if (tail == nullptr)
+		if (head == nullptr)
 		{
 			head = newNode;
-			tail = newNode;
+
+			newNode->next = head;
 		}
 		else
 		{
-			tail->next = newNode;
+			newNode->next = head->next;
 
-			newNode->previous = tail;
+			head->next = newNode;
 
-			tail = newNode;
+			head = newNode;
 		}
 
 		size++;
 	}
 
-	const bool& empty()
+	void pop_back()
 	{
-		return head == nullptr;
-	}
-
-	~List()
-	{
-		while (head != nullptr)
+		if (head == nullptr)
 		{
-			pop_front();
+			cout << "linked list is empty" << endl;
+		}
+		else
+		{
+
 		}
 	}
 };
@@ -121,13 +64,6 @@ int main()
 {
 	List<int> list;
 
-	list.push_front(10);
-	list.push_front(5);
-
+	list.push_back(10);
 	list.push_back(20);
-	list.push_back(30);
-
-	list.pop_front();
-	list.pop_front();
-	list.pop_front();
 }
