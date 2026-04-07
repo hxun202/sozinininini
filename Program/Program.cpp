2a42592a003c261136c6a59fceef16d17ae415bf
hxun202 <kangsojin0918@gmail.com>
2026-04-07 21:44:51 +0900

    2026.4.7 균형 이진 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -73,6 +73,89 @@ public:
 			}
 		}
 	}
+
+	void erase(T data)
+	{
+		Node* currentNode = root;
+		Node* parentNode = nullptr;
+
+		while (currentNode != nullptr && currentNode->data != data)
+		{
+			parentNode = currentNode;
+
+			if (currentNode->data > data)
+			{
+				currentNode = currentNode->left;
+			}
+			else
+			{
+				currentNode = currentNode->right;
+			}
+		}
+
+		if (currentNode == nullptr)
+		{
+			cout << "the data does not exist" << endl;
+
+			return;
+		}
+		else if (currentNode->left == nullptr && currentNode->right == nullptr)
+		{
+			if (parentNode != nullptr)
+			{
+				if (parentNode->left == currentNode)
+				{
+					parentNode->left = nullptr;
+				}
+				else
+				{
+					parentNode->right = nullptr;
+				}
+			}
+			else
+			{
+				root = nullptr;
+			}
+		}
+		else if (currentNode->left != nullptr || currentNode->right != nullptr)
+		{
+			if (currentNode == root)
+			{
+				if (currentNode->left != nullptr)
+				{
+					root = currentNode->left;
+				}
+				else
+				{
+					root = currentNode->right;
+				}
+			}
+			else
+			{
+				Node* childNode = nullptr;
+
+				if (currentNode->left != nullptr)
+				{
+					childNode = currentNode->left;
+				}
+				else
+				{
+					childNode = currentNode->right;
+				}
+
+				if (parentNode->left == currentNode)
+				{
+					parentNode->left = childNode;
+				}
+				else
+				{
+					parentNode->right = childNode;
+				}
+			}
+		}
+
+		delete currentNode;
+	}
 };
 
 int main()
@@ -83,6 +166,11 @@ int main()
 	set.insert(15);
 	set.insert(6);
 	set.insert(12);
+
+	set.erase(2);
+	set.erase(1);
+	set.erase(3);
+	set.erase(3);
 
 	return 0;
 }
